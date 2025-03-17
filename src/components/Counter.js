8b33754c883845c33f60d8d00a33b2d65f66af1e
vexior27 <jakubmalec07@gmail.com>
2025-03-17 16:59:04 +0100

    added buttons and functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,48 @@
-
-import React, {useState} from "react";
-import {Text, Button, Container, NumberInput} from "@mantine/core"
+import React, { useState } from "react";
+import {
+   Title,
+   Button,
+   Container,
+   NumberInput,
+   Flex,
+   Stack,
+   Center,
+} from "@mantine/core";
+import { IconPlus, IconMinus } from "@tabler/icons-react";
 
 export default function Counter() {
+
+   const [counter, setCounter] = useState(0);
+
+   const add = () => {
+      setCounter(counter + 1);
+   }
+
+   const substract = () => {
+      setCounter(counter - 1);
+   }
+
    return (
       <Container>
-         <Text size="xl">React Counter</Text>
-         <NumberInput 
-            size="xl"
-         />
-         <Button
-            size="xl"
-         >Add
-         </Button>
-         <Button
-            size="xl"
-         >
-            Minus
-         </Button>
+         <Stack gap="xl">
+            <Center>
+               <Title order={1} c='blue'>React Counter</Title>
+            </Center>
+            <Center>
+               <NumberInput size="xl" style={{ width: 200 }} value={counter} onChange={(e) => setCounter(e)}/>
+            </Center>
+            <Flex gap="md" justify="center" align="center" direction="row">
+               <Button size="xl" onClick={() => add()}>
+                  <IconPlus />
+               </Button>
+               <Button size="xl" onClick={() => substract()}>
+                  <IconMinus />
+               </Button>
+               <Button size="xl" onClick={() => setCounter(0)}>
+                  Reset
+               </Button>
+            </Flex>
+         </Stack>
       </Container>
-   )
+   );
 }
